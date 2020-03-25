2fd78db029231ce7090209e819467b943b898eca
DaNicolescu <daniel.nicolescu10@gmail.com>
2020-03-25 19:59:37 +0200

    added debug mode

--- client/client.py
+++ client/client.py
@@ -83,4 +83,4 @@ def process_input():
             print("Invalid input")
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
--- server/server.py
+++ server/server.py
@@ -9,4 +9,4 @@ def return_msg():
     return "shit"
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5001)
+    app.run('0.0.0.0', port=5001, debug=True)
