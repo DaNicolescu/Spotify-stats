04a1bee98214292a28e1d2b4b15fc3c30212349d
DaNicolescu <daniel.nicolescu10@gmail.com>
2020-03-28 18:13:56 +0200

    added the Spotify interface

--- client/client.py
+++ client/client.py
@@ -9,21 +9,25 @@ app = Flask(__name__)
 def homepage():
     return render_template("index.html")
 
-@app.route("/<artist_name>")
-def display_info(artist_name):
-    return f"<h1>{artist_name}</h1>"
-
 @app.route('/artists', methods=['GET', 'POST'])
 def artists():
     if request.method == 'GET':
         return render_template("artists.html")
     else:
-        artist = request.form['artist_name']
-        return redirect(url_for("display_info", artist_name=artist))
+        artist = request.form["artist_name"]
+        response = requests.post("http://server:5001/artist_info", json={"artist_name":artist})
+        response_data = response.json()
+        return render_template("artist_info.html", artist_name=response_data["artist_name"], followers=response_data["followers"], genres=response_data["genres"], popularity=response_data["popularity"], image=response_data["image"], top_tracks=response_data["top_tracks"], album_names=response_data["album_names"])
 
-@app.route('/tracks')
+@app.route('/tracks', methods=['GET', 'POST'])
 def tracks():
-    return render_template("tracks.html")
+    if request.method == 'GET':
+        return render_template("tracks.html")
+    else:
+        track = request.form['track_name']
+        response = requests.post("http://server:5001/track_info", json={"track_name":track})
+        response_data = response.json()
+        return render_template("track_info.html", track_name=track, album=response_data["album"], popularity=response_data["popularity"], duration=response_data["duration"])
 
 @app.route('/recommendations')
 def reccommendations():
--- server/server.py
+++ server/server.py
@@ -1,9 +1,13 @@
 from flask import Flask, request, jsonify
 import json
 import mysql.connector
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
 app = Flask(__name__)
 
+client_credentials_manager = SpotifyClientCredentials(client_id="adef179cbfaf472281e7f19291a20586", client_secret="d003550df96c4e49b9781018f1df709b")
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 def create_table():
     airplaneService_db = mysql.connector.connect(host='db', port='3306'
@@ -14,6 +18,26 @@ def create_table():
     airplaneService_cursor.execute('CREATE TABLE test (ID VARCHAR(255), Source VARCHAR(255), Dest VARCHAR(255), DepartureDay INT, DepartureHour INT, Duration INT, NumberofSeats INT, NumberofSeats_booked INT)'
                                    )
 
+@app.route('/artist_info', methods=['GET','POST'])
+def get_artist_info():
+	request_info = request.get_json(silent=True)
+	result = sp.search(request_info["artist_name"], type="artist")
+	artist = result["artists"]["items"][0]
+	top_tracks = sp.artist_top_tracks(artist["uri"])
+
+	top_tracks_names = []
+	top_tracks_album_names = []
+
+	for track in top_tracks["tracks"]:
+		top_tracks_names.append(track["name"])
+		top_tracks_album_names.append(track["album"]["name"])
+
+	return jsonify({"artist_name": artist["name"], "followers": artist["followers"]["total"], "genres": artist["genres"], "popularity": artist["popularity"], "image": artist["images"][0]["url"], "top_tracks": top_tracks_names, "album_names": top_tracks_album_names})
+
+@app.route('/track_info', methods=['GET','POST'])
+def get_track_info():
+	request_info = request.get_json(silent=True)
+	return jsonify({"track_name": request_info["track_name"], "album": "Nicolae Guță best of", "popularity": "100", "duration": "4:20"})
 
 @app.route('/hello', methods=['GET', 'POST'])
 def return_msg():
@@ -28,6 +52,11 @@ def return_server_ok():
     if request.method == 'POST':
         return jsonify({'server': 'merge serverul, boss'})
     return 'merge ceva'
+
+@app.route('/test_spot', methods=['GET', 'POST'])
+def check_spotipy():
+	result = sp.artist_top_tracks("spotify:artist:12Chz98pHFMPJEknJQMWvI")
+	return result
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
