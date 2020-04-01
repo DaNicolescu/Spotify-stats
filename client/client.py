c3d7c4aea20566207d8d5e06885ab88af8f218da
DaNicolescu <daniel.nicolescu10@gmail.com>
2020-04-01 22:42:18 +0300

    added the Tracks functionality

--- client/client.py
+++ client/client.py
@@ -15,9 +15,17 @@ def artists():
         return render_template("artists.html")
     else:
         artist = request.form["artist_name"]
-        response = requests.post("http://server:5001/artist_info", json={"artist_name":artist})
+        response = requests.post("http://server:5001/artist_info",
+        	json={"artist_name":artist})
         response_data = response.json()
-        return render_template("artist_info.html", artist_name=response_data["artist_name"], followers=response_data["followers"], genres=response_data["genres"], popularity=response_data["popularity"], image=response_data["image"], top_tracks=response_data["top_tracks"], album_names=response_data["album_names"])
+        return render_template("artist_info.html",
+        	artist_name=response_data["artist_name"],
+        	followers=response_data["followers"],
+        	genres=response_data["genres"],
+        	popularity=response_data["popularity"],
+        	image=response_data["image"],
+        	top_tracks=response_data["top_tracks"],
+        	album_names=response_data["album_names"])
 
 @app.route('/tracks', methods=['GET', 'POST'])
 def tracks():
@@ -25,91 +33,26 @@ def tracks():
         return render_template("tracks.html")
     else:
         track = request.form['track_name']
-        response = requests.post("http://server:5001/track_info", json={"track_name":track})
+        response = requests.post("http://server:5001/track_info",
+        	json={"track_name":track})
         response_data = response.json()
-        return render_template("track_info.html", track_name=track, album=response_data["album"], popularity=response_data["popularity"], duration=response_data["duration"])
+        return render_template("track_info.html",
+        	track_name=response_data["track_name"],
+        	artist_name=response_data["artist_name"],
+        	album=response_data["album"],
+        	album_photo=response_data["album_photo"],
+        	release_date=response_data["release_date"],
+        	popularity=response_data["popularity"],
+        	duration=response_data["duration"])
 
 @app.route('/recommendations')
-def reccommendations():
+def recommendations():
     return render_template("recommendations.html")
 
 @app.route('/server')
 def check_server():
     response = requests.post("http://server:5001/boss")
     return response.json()['server']
-
-def get_artist_info(artist_name):
-    # use search for an item from Spotify API to get info about an artist
-    print("Artist name: -")
-    print("Popularity: -")
-    print("Followers: -")
-    print("Genres: -")
-
-def get_artist_top_tracks(artist_name):
-    # use search for an item to get artist's ID and then use it as a param
-    # for get an artist's top tracks
-    print("Track 1")
-    print("Track 2")
-    print("Track 3")
-
-def get_track_info(track_name):
-    # use search to get the track's ID and then use get a track
-    print("Track name: -")
-    print("Album: -")
-    print("Popularity: -")
-    print("Duration: -")
-
-def get_suggestions(genre):
-    print("Suggestion 1")
-    print("Suggestion 2")
-    print("Suggestion 3")
-
-def process_input():
-    while True:
-        print("-----------------------------")
-        print("Available Commands:")
-        print("1. Get artist info")
-        print("2. Get artist's top tracks")
-        print("3. Get track info")
-        print("4. Get suggestions")
-        print("5. Exit")
-        print("-----------------------------")
-
-        input = sys.stdin.readline()
-        input = input.strip('\n')
-
-        if int(input) == 1:
-            print("Enter artist name:")
-            artist_name = sys.stdin.readline()
-            artist_name = artist_name.strip('\n')
-            artist_name = artist_name.lower()
-
-            get_artist_info(artist_name)
-        elif int(input) == 2:
-            print("Enter artist name:")
-            artist_name = sys.stdin.readline()
-            artist_name = artist_name.strip('\n')
-            artist_name = artist_name.lower()
-
-            get_artist_top_tracks(artist_name)
-        elif int(input) == 3:
-            print("Enter track name:")
-            track_name = sys.stdin.readline()
-            track_name = track_name.strip('\n')
-            track_name = track_name.lower()
-
-            get_track_info(track_name)
-        elif int(input) == 4:
-            print("Enter genre:")
-            genre = sys.stdin.readline()
-            genre = genre.strip('\n')
-            genre = genre.lower()
-
-            get_suggestions(genre)
-        elif int(input) == 5:
-            break
-        else:
-            print("Invalid input")
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
--- server/server.py
+++ server/server.py
@@ -6,17 +6,10 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 app = Flask(__name__)
 
-client_credentials_manager = SpotifyClientCredentials(client_id="adef179cbfaf472281e7f19291a20586", client_secret="d003550df96c4e49b9781018f1df709b")
+client_credentials_manager = SpotifyClientCredentials(
+	client_id="adef179cbfaf472281e7f19291a20586",
+	client_secret="d003550df96c4e49b9781018f1df709b")
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-
-def create_table():
-    airplaneService_db = mysql.connector.connect(host='db', port='3306'
-            , user='user', passwd='parola', database='test')
-
-    airplaneService_cursor = airplaneService_db.cursor()
-
-    airplaneService_cursor.execute('CREATE TABLE test (ID VARCHAR(255), Source VARCHAR(255), Dest VARCHAR(255), DepartureDay INT, DepartureHour INT, Duration INT, NumberofSeats INT, NumberofSeats_booked INT)'
-                                   )
 
 @app.route('/artist_info', methods=['GET','POST'])
 def get_artist_info():
@@ -32,19 +25,29 @@ def get_artist_info():
 		top_tracks_names.append(track["name"])
 		top_tracks_album_names.append(track["album"]["name"])
 
-	return jsonify({"artist_name": artist["name"], "followers": artist["followers"]["total"], "genres": artist["genres"], "popularity": artist["popularity"], "image": artist["images"][0]["url"], "top_tracks": top_tracks_names, "album_names": top_tracks_album_names})
+	return jsonify({"artist_name": artist["name"],
+		"followers": artist["followers"]["total"],
+		"genres": artist["genres"],
+		"popularity": artist["popularity"],
+		"image": artist["images"][0]["url"],
+		"top_tracks": top_tracks_names,
+		"album_names": top_tracks_album_names})
 
 @app.route('/track_info', methods=['GET','POST'])
 def get_track_info():
 	request_info = request.get_json(silent=True)
-	return jsonify({"track_name": request_info["track_name"], "album": "Nicolae Guță best of", "popularity": "100", "duration": "4:20"})
+	result = sp.search(request_info["track_name"], type="track")
+	track_info = result["tracks"]["items"][0]
+	artist_name = track_info["artists"][0]["name"]
+	track_duration = str(round(((track_info["duration_ms"] * 1.0) / (1000 * 60)) % 60, 2))
 
-@app.route('/hello', methods=['GET', 'POST'])
-def return_msg():
-    if request.method == 'POST':
-        return jsonify({'pula': 'pula_da'})
-    return 'get method'
-
+	return jsonify({"track_name": track_info["name"],
+		"artist_name": track_info["artists"][0]["name"],
+		"album": track_info["album"]["name"],
+		"album_photo": track_info["album"]["images"][0]["url"],
+		"release_date": track_info["album"]["release_date"],
+		"popularity": track_info["popularity"],
+		"duration": track_duration})
 
 @app.route('/boss', methods=['GET', 'POST'])
 def return_server_ok():
